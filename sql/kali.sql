ad46a1bb1365f42087d70416d04c5f5f21b0a9b7
a16888861 <2271998412@qq.com>
2021-07-09 21:23:19 +0800

    更新数据库表

--- sql/kali.sql
+++ sql/kali.sql
@@ -11,31 +11,35 @@
  Target Server Version : 50710
  File Encoding         : 65001
 
- Date: 13/06/2021 22:39:58
+ Date: 09/07/2021 21:21:40
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for admin
+-- Table structure for kali_user
 -- ----------------------------
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin` (
-  `aid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `password` varchar(64) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `status` int(2) DEFAULT '1',
-  `lend_num` int(11) DEFAULT NULL,
-  `max_num` int(11) DEFAULT NULL,
-  PRIMARY KEY (`aid`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+DROP TABLE IF EXISTS `kali_user`;
+CREATE TABLE `kali_user` (
+  `uid` varchar(32) NOT NULL COMMENT '标识符',
+  `name` varchar(32) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8 DEFAULT NULL COMMENT '密码',
+  `mail` varchar(200) CHARACTER SET utf8 DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(20) CHARACTER SET utf8 DEFAULT NULL COMMENT '手机号',
+  `screenName` varchar(32) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户名-别名',
+  `group` varchar(32) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户组别',
+  `year` varchar(20) CHARACTER SET utf8 DEFAULT NULL COMMENT '年份',
+  `create_by` varchar(32) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(32) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人',
+  `update_date` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8 DEFAULT NULL COMMENT '删除状态',
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='用户表';
 
 -- ----------------------------
--- Records of admin
+-- Records of kali_user
 -- ----------------------------
 BEGIN;
 COMMIT;
