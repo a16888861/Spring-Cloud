490b28bfabe7922d528eb8d4d81085b18335d697
a16888861 <2271998412@qq.com>
2021-08-24 23:39:28 +0800

    更新SQL文件

--- sql/kali.sql
+++ sql/kali.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50705
  File Encoding         : 65001
 
- Date: 22/08/2021 22:49:16
+ Date: 24/08/2021 23:38:47
 */
 
 SET NAMES utf8mb4;
@@ -39,6 +39,42 @@ CREATE TABLE `kali_group` (
 -- Records of kali_group
 -- ----------------------------
 BEGIN;
+INSERT INTO `kali_group` VALUES ('1430106533911797760', '9999', 'superAdmin', NULL, '2021', NULL, '2021-08-24 17:55:42', NULL, NULL, '0');
+INSERT INTO `kali_group` VALUES ('1430109065098715136', '9998', 'admin', NULL, '2021', NULL, '2021-08-24 18:05:46', NULL, NULL, '0');
+INSERT INTO `kali_group` VALUES ('1430109104843939840', '1000', 'user', NULL, '2021', NULL, '2021-08-24 18:05:55', NULL, NULL, '0');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for kali_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `kali_menu`;
+CREATE TABLE `kali_menu` (
+                             `id` varchar(32) NOT NULL COMMENT '唯一标识',
+                             `sys_id` varchar(32) DEFAULT NULL COMMENT '系统id',
+                             `parent_id` varchar(32) DEFAULT NULL COMMENT '父级id',
+                             `parent_code` varchar(20) DEFAULT NULL COMMENT '父级code',
+                             `parent_ids` varchar(1000) DEFAULT NULL COMMENT '上级ids',
+                             `name` varchar(50) NOT NULL COMMENT '菜单名称',
+                             `href` varchar(200) DEFAULT '' COMMENT '菜单访问路径',
+                             `target` varchar(30) DEFAULT NULL COMMENT '菜单打开方式',
+                             `icon` varchar(30) DEFAULT NULL COMMENT '图标',
+                             `sort` varchar(10) DEFAULT NULL COMMENT '排序',
+                             `code` varchar(20) DEFAULT NULL COMMENT '菜单编码',
+                             `status` varchar(10) DEFAULT NULL COMMENT '菜单状态',
+                             `is_show` char(1) NOT NULL COMMENT '是否显示',
+                             `type` varchar(10) DEFAULT NULL COMMENT '菜单类型',
+                             `create_by` varchar(32) NOT NULL COMMENT '创建人',
+                             `create_date` datetime NOT NULL COMMENT '创建时间',
+                             `update_by` varchar(32) DEFAULT NULL COMMENT '修改人',
+                             `update_date` datetime DEFAULT NULL COMMENT '修改时间',
+                             `del_flag` char(1) NOT NULL COMMENT '删除标志',
+                             PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='系统-菜单表';
+
+-- ----------------------------
+-- Records of kali_menu
+-- ----------------------------
+BEGIN;
 COMMIT;
 
 -- ----------------------------
@@ -50,7 +86,7 @@ CREATE TABLE `kali_role` (
                              `sys_id` varchar(32) NOT NULL COMMENT '系统id',
                              `code` varchar(20) DEFAULT NULL COMMENT '角色代码',
                              `name` varchar(50) NOT NULL COMMENT '角色名称',
-                             `enname` varchar(50) DEFAULT NULL COMMENT '角色英文名称',
+                             `en_name` varchar(50) DEFAULT NULL COMMENT '角色英文名称',
                              `type` varchar(10) NOT NULL COMMENT '角色类型',
                              `status` varchar(10) NOT NULL COMMENT '状态',
                              `data_permission_type` varchar(50) DEFAULT NULL COMMENT '数据权限',
@@ -66,6 +102,26 @@ CREATE TABLE `kali_role` (
 
 -- ----------------------------
 -- Records of kali_role
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for kali_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `kali_role_menu`;
+CREATE TABLE `kali_role_menu` (
+                                  `id` varchar(32) NOT NULL COMMENT '唯一标识',
+                                  `role_id` varchar(32) NOT NULL COMMENT '角色id',
+                                  `menu_id` varchar(32) NOT NULL COMMENT '菜单id',
+                                  `element_id` varchar(32) DEFAULT NULL COMMENT '元素id',
+                                  PRIMARY KEY (`id`) USING BTREE,
+                                  KEY `idx_0` (`role_id`,`menu_id`) USING BTREE,
+                                  KEY `idx_1` (`menu_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='系统-角色菜单表';
+
+-- ----------------------------
+-- Records of kali_role_menu
 -- ----------------------------
 BEGIN;
 COMMIT;
@@ -96,7 +152,8 @@ CREATE TABLE `kali_user` (
 -- Records of kali_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `kali_user` VALUES ('1424295941559369728', 'aaa', '@6y!@2&8@6fyÒ!2c@2af5@°°fa$$0&&2', '123', '13131', 'zhangsan', '9999', NULL, '2021', 'admin', '2021-08-08 17:06:29', NULL, NULL, '0');
+INSERT INTO `kali_user` VALUES ('1430066935624323072', '张泱森', '@6y!@2&8@6fyÒ!2c@2af5@°°fa$$0&&2', '2271998412@qq.com', '18234125116', '阿森', '9999', NULL, '2021', '1430109634181881856', '2021-08-24 15:18:21', NULL, NULL, '0');
+INSERT INTO `kali_user` VALUES ('1430109634181881856', '超级管理员', '°c2◊20a620&@582a@yÒ6!y2ya!ay$fÒ$', 'admin@mail.com', '18888888888', 'SuperAdmin', '9999', NULL, '2021', '', '2021-08-24 18:08:02', NULL, NULL, '0');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
