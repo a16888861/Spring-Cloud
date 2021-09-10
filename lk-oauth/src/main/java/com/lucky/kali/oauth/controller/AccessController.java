8e543877f7e9237abe84867ceadc5370efa37d3a
a16888861 <2271998412@qq.com>
2021-09-11 00:42:28 +0800

    新增Redis工具类 鉴权部分思路变动，暂未设置

--- lk-oauth/src/main/java/com/lucky/kali/oauth/controller/AccessController.java
+++ lk-oauth/src/main/java/com/lucky/kali/oauth/controller/AccessController.java
@@ -1,23 +1,28 @@
 package com.lucky.kali.oauth.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import com.lucky.kali.common.response.Response;
 import com.lucky.kali.common.response.ResponseEnum;
 import com.lucky.kali.common.response.ResponseInfo;
+import com.lucky.kali.common.util.JwtUtil;
 import com.lucky.kali.common.util.Md5Utils;
+import com.lucky.kali.common.util.RedisUtil;
 import com.lucky.kali.common.vo.req.LoginVO;
 import com.lucky.kali.common.vo.resp.UserInfoVO;
 import com.lucky.kali.oauth.fegin.UserFegin;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Elliot
@@ -28,8 +33,11 @@ import javax.validation.Valid;
 @Slf4j
 public class AccessController {
 
+    private TokenEndpoint tokenEndpoint;
     @Resource
     private UserFegin userFegin;
+    @Resource
+    private RedisUtil redisUtil;
 
     @PostMapping("doLogin")
     @ApiOperation(value = "用户登陆", produces = "application/json",
@@ -42,6 +50,7 @@ public class AccessController {
                     "password: TOBENO.1",
             position = 1)
     @ApiOperationSupport(author = "Elliot")
+//    public ResponseInfo<UserInfoVO> doLogin(Principal principal, @Valid LoginVO loginVO, BindingResult bindingResult) {
     public ResponseInfo<UserInfoVO> doLogin(@Valid LoginVO loginVO, BindingResult bindingResult) {
         log.info("登陆信息为：" + loginVO);
         if (bindingResult.hasErrors()) {
@@ -64,13 +73,11 @@ public class AccessController {
         if (userInfo.getStatusCode() == ResponseEnum.NOTFOUND.getCode()) {
             return Response.notFound(ResponseEnum.NOTFOUND.getMessage());
         }
-//        String token = JwtUtil.createToken(JSONUtil.toJsonStr(userDTO));
-
-        //TODO Token待存放到Redis中
-//        UserDTO userDTO1 = JSONUtil.toBean(JwtUtil.getClaim(token), UserDTO.class);
-//        log.info(userDTO1.toString());
-//        log.info(token);
-//        log.info(JwtUtil.getClaim(token));
+//        OAuth2AccessToken oAuth2AccessToken = tokenEndpoint.postAccessToken(principal, parameters).getBody();
+        /*Token存放到Redis中*/
+        String token = JwtUtil.createToken(JSONUtil.toJsonStr(userInfo));
+        /*存储Token信息(2小时后过期)*/
+        redisUtil.set("userInfo:" + userInfo.getData().getUserDTO().getPhone(), token, TimeUnit.SECONDS.toSeconds(7200));
         return userInfo;
     }
 }
