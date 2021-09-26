c87b32273603ffc89cf41f69d0eb6f000febccfc
Elliot <2271998412@qq.com>
2021-09-26 17:26:27 +0800

    测试

--- lk-common/lk-util-common/src/main/java/com/lucky/kali/common/Scheduled/CommonScheduled.java
+++ lk-common/lk-util-common/src/main/java/com/lucky/kali/common/Scheduled/CommonScheduled.java
@@ -1,5 +1,6 @@
 package com.lucky.kali.common.Scheduled;
 
+import lombok.SneakyThrows;
 import org.jboss.logging.Logger;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,9 +18,10 @@ public class CommonScheduled {
     /**
      * 每60秒执行一次
      */
+    @SneakyThrows
     @Scheduled(cron = "0/60 * * * * ? ")
     public void aTask() {
         /*测试任务*/
-//        log.info("********* 现在时间是 " + LocalDateTimeUtil.localDateTimeToDate(LocalDateTime.now()) + " *********");
+//        log.info("********* 现在主机：" + InetAddress.getLocalHost().toString() + "的时间是 " + LocalDateTimeUtil.localDateTimeToDate(LocalDateTime.now()) + " *********");
     }
 }
